02473e1fa5d83be6bd763fd8e1ecc4341daef9c8
Garrett Merz <garrettwmerz@gmail.com>
2024-12-06 12:22:47 -0600

    packaging

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='AIAmplitudes',
+    packages=find_packages(include=['src/AIAA_common']),
+    version='0.0.2',
+    description='AIAA common library',
+    author='Garrett Merz',
+)
