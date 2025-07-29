2ebd068a0b0803751680691e89caadad2708fe28
Garrett Merz <garrettwmerz@gmail.com>
2025-07-29 17:13:57 -0500

    updates to public/private file storage workflow

--- src/aiamplitudes_common_public/download_data.py
+++ src/aiamplitudes_common_public/download_data.py
@@ -6,15 +6,14 @@ import json
 import os
 import requests
 from pathlib import Path
-
-username= None
-password = None
+import shutil
+import git
 
 ################### Download tarballs from git ###############################
-public_repo =  "AIAmplitudes/data_public"
-private_repo =  "AIAmplitudes/data_private"
+public_repo =  "AIAmplitudes/Phase1_data"
+private_repo =  "AIAmplitudes/data_public"
 
-def _cache_path(cache_dir: str | None = None) -> Path:
+def _cache_path(cache_dir: str | None = None, make_tarfdir = True) -> Path:
     if cache_dir is None:
         ampdir = Path.home() / ".local" / "AIAmplitudesData"
         try:
@@ -24,9 +23,17 @@ def _cache_path(cache_dir: str | None = None) -> Path:
             os.mkdir('./cache')
             ampdir='./cache'
         return Path(ampdir)
+
+    if make_tarfdir:
+        tarfdir = ampdir / "tarfiles"
+        tarfdir.mkdir(exist_ok=True, parents=True)
+
     return Path(cache_dir)
 
 relpath = _cache_path(None)
+
+def clear_cache():
+    os.system(f'rm -rf {relpath}')
 
 def get_gitfilenames(the_zipurl):
     soup = BeautifulSoup(requests.get(the_zipurl).text)
@@ -36,7 +43,7 @@ def get_gitfilenames(the_zipurl):
             files += [i["name"] for i in json.loads(elem.contents[0])["props"]["initialPayload"]["tree"]["items"]]
     return files
 
-def download_unpack(myfile: str, local_dir: Path):
+def download_unpack(myfile: str, local_dir: Path, tarfdir = None):
     with tempfile.TemporaryFile() as f:
         with requests.get(myfile, stream=True) as r:
             for chunk in r.iter_content(chunk_size=8192):
@@ -46,6 +53,8 @@ def download_unpack(myfile: str, local_dir: Path):
             for n in tarf.getnames():
                 assert os.path.abspath(os.path.join(local_dir, n)).startswith(str(local_dir))
             tarf.extractall(path=local_dir)
+        if tarfdir: shutil.move(f"{str(local_dir)}/{myfile}",
+                                f"{str(tarfdir)}/{f}")
     return
 
 def download_all_public(repo: str = public_repo, cache_dir: str | None = None) -> None:
@@ -64,6 +73,8 @@ def download_all_public(repo: str = public_repo, cache_dir: str | None = None) -
 
     #dump all files into the root directory
     for subdir, dirs, files in os.walk(local_dir):
+        if "tarfiles" in subdir: continue
+        if ".git" in subdir: continue
         for file in files:
             os.rename(str(os.path.join(subdir, file)),str(os.path.join(local_dir, file)))
 
@@ -80,16 +91,61 @@ def download_all_public(repo: str = public_repo, cache_dir: str | None = None) -
             deleted.add(thisdir)
     return
 
-def download_all_private(mytoken, repo: str = public_repo, cache_dir: str | None = None) -> None:
+def download_all_private(username,mytoken, repo: str = public_repo, cache_dir: str | None = None) -> None:
     #use the github CLI and token access to get datasets in the private repo
-    g = Github(token)
-    repo_dir = relpath
-    git_url = "https://github.com/AIAmplitudes/data_public.git"
-    Repo.clone_from(git_url, repo_dir)
+    local_dir = _cache_path(cache_dir)
+    git_url = f"https://{username}:{mytoken}@github.com/AIAmplitudes/data_public.git"
+    git.Repo.clone_from(git_url, local_dir)
+    tarfdir = local_dir / "tarfiles"
+    tarfdir.mkdir(exist_ok=True, parents=True)
+    for f in os.listdir(local_dir):
+        if ".tar" in f:
+            mytar = f"{local_dir}/{f}"
+            with tarfile.open(mytar) as tarf:
+                for n in tarf.getnames():
+                    assert os.path.abspath(os.path.join(local_dir, n)).startswith(str(local_dir))
+                tarf.extractall(path=local_dir)
+            shutil.move(mytar, f"{str(tarfdir)}/{f}")
+
+    #dump all files into the root directory
+    for subdir, dirs, files in os.walk(local_dir):
+        if ".git" in subdir: continue
+        if "tarfiles" in subdir: continue
+        for file in files:
+            os.rename(str(os.path.join(subdir, file)),str(os.path.join(local_dir, file)))
+
+    #delete all subdirs that do not have a file in them
+    deleted=set()
+
+    for thisdir, subdirs, files in os.walk(local_dir, topdown=False):
+        if "tarfiles" in str(thisdir): continue
+        if ".git" in str(thisdir): continue
+        still_has_subdirs = False
+        for subdir in subdirs:
+            if os.path.join(thisdir, subdir) not in deleted:
+                still_has_subdirs = True
+                break
+        if not any(files) and not still_has_subdirs:
+            os.rmdir(thisdir)
+            deleted.add(thisdir)
     return
 
-def download_all(mytoken = None):
-    if mytoken: download_all_private(mytoken)
-    download_all_public()
+def download_all(username= None, mytoken = None):
+    if len(os.listdir(relpath)) != 0:
+        clear_cache()
+    if not username or not mytoken:
+        username = input("Github user:")
+        mytoken = input("Github access token:")
+    try:
+        download_all_private(username, mytoken)
+    except git.exc.GitCommandError:
+        print("github access error- defaulting to phase 1 data download only")
+        download_all_public()
     return
+
+
+
 #######################################################################################
+
+if __name__ == "__main__":
+    download_all()
